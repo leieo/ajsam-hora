888ec132ca813b8e20178d8f29fa5499448ce0fb
leieo <leila.el.omari@gmail.com>
2018-02-13 22:46:43 +0100

    mise en place structure Vue & tests typo

---
+++ ajsam-hora/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <div class="section columns">
+      <div class="column is-one-third">
+        <project>Multicorps</project>
+      </div>
+      <div class="column is-one-third">
+        <project>Vertikal</project>
+      </div>
+      <div class="column is-one-third">
+        <project>NissiNila</project>
+      </div>
+    </div>
+<!--
+    <h3>{{ projectTitle }}</h3>
+    <h4>{{ memberName }}</h4>
+    <br>
+
+    <h3>{{ projectTitle }}</h3>
+    <h2>{{ memberName }}</h2>
+    <br>
+
+    <h5>{{ projectTitle }}</h5>
+  -->
+
+    <button class="button">test</button>
+  </div>
+</template>
+
+<script>
+import Project from './components/Project.vue';
+export default {
+  name: 'app',
+  components: { Project },
+  data () {
+    return {
+      msg: 'Hello World',
+      title: 'Ajsam horA',
+      projectTitle: 'Multicorps'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  text-align: center;
+}
+
+h1 {
+  font-family: 'Cabin Sketch', Helvetica, Arial, sans-serif;
+  font-size: 55px;
+  letter-spacing: 5px;
+  padding: 30px;
+}
+
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ ajsam-hora/src/components/Project.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+		<div class="frame">
+		    <h3>
+		    	<slot></slot>
+		    </h3>
+		</div>
+	    <h2>{{ memberName }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			memberName: 'Sandie Brichler'
+		}
+	}
+}
+</script>
+
+<style>
+	
+h2 {
+  font-family: 'Nova Mono', Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  font-weight: 100;
+  text-transform: uppercase;
+  letter-spacing: 4px;
+}
+
+h3 {
+  font-family:'Waiting for the Sunrise', Helvetica, Arial, sans-serif;
+  font-size: 32px;
+  letter-spacing: 10px;
+}
+
+h4 {
+  font-family: 'Bungee Hairline', Helvetica, Arial, sans-serif;
+  word-spacing: 10px;
+  font-size: 18px;
+}
+
+h5 {
+  font-family: 'Special Elite', Helvetica, Arial, sans-serif;
+  font-size: 25px;
+  letter-spacing: 8px;
+}
+
+.frame  {
+  border: 1px solid black;
+  padding: 50px 50px;
+  margin: 16px;
+}
+</style>
+
