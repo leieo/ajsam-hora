f43ab15b209a514fe366b0822f259888504fa024
leieo <leila.el.omari@gmail.com>
2018-02-15 17:49:57 +0100

    pour chaque projet, image+nom & toggle image visible / invisible - tests

--- grid.js
+++ grid.js
@@ -1,14 +1,12 @@
 Vue.component('creations', {
 	template: `
 	    <div class="section columns"> 
-	      <div class="column is-1-tablet is-2-desktop is-one-fifth-widescreen is-3-fullhd reveal">
-			<input type="checkbox" v-model="ishidden">
-		  </div>
+	      <div class="column is-1-tablet is-2-desktop is-one-fifth-widescreen is-3-fullhd reveal"></div>
 	      <div class="column">
 	        <div class="columns is-multiline">
-	        	<creation v-for="creation in creations" :key="creation.name" class="column is-one-third">
-	        		<img :src="creation.imgurl" class="is-overlay level-item" :class="clstoto">
-					<h3 class="is-overlay level-item">{{ creation.name }}</h3>
+	        	<creation v-for="creation in creations" :key="creation.name" class="column is-one-third" v-on:shwtxt="showText" v-on:shwimg="showImage">
+	        		<img :src="creation.imgurl" class="is-overlay level-item" v-if="imageVisible">
+					<h3 class="is-overlay level-item" v-if="textVisible">{{ creation.name }}</h3>
 	        	</creation>
 	        </div>
 	      </div>
@@ -18,17 +16,27 @@ Vue.component('creations', {
 
 	data () {
 		return {
-			ishidden: false,
+			imageVisible: true,
+			textVisible: false,
 			creations: [
 				{ id: 1, name: 'Banja Luka', imgurl: './ajsam-hora/src/img/banjaluka.jpg'},
 				{ id: 2, name: 'Nissi Nila', imgurl: './ajsam-hora/src/img/nissinila.jpg'},
+				{ id: 2, name: 'Floating In Red', imgurl: './ajsam-hora/src/img/floatinginred.jpg'},
+				{ id: 2, name: 'Multicorps', imgurl: './ajsam-hora/src/img/multicorps.jpg'},
 			]
 		};
 	},
-	computed: {
-		clstoto: function () {
-			console.log('clstoto called')
-			return this.ishidden === true ? 'is-invisible' : 'is-blue'
+
+	methods: {
+		showText () {
+			console.log('Le texte apparaît')
+			this.imageVisible = false
+			this.textVisible = true
+		},
+		showImage () {
+			console.log('L\'image apparaît')
+			this.imageVisible = true
+			this.textVisible = false
 		}
 	}
 });
@@ -36,41 +44,47 @@ Vue.component('creations', {
 Vue.component('creation', {
 	template: `
 	<div>
-		    <figure class="image is-3by2 level framed">
+		    <figure class="image is-3by2 level framed" @mouseover="shwtxt" @mouseout="shwimg">
 		    	<slot></slot>
 		    </figure>
 	</div>
-	`
+	`,
+	methods: {
+		shwtxt () {
+			console.log('shwtxt directive')
+			this.$emit('shwtxt')
+		},
+		shwimg () {
+			console.log('shwimg directive')
+			this.$emit('shwimg')
+		}
+	}
 });
 
 new Vue({
 	el: '#root',
 	data: {
+		imageVisible: true,
+		textVisible: false,
 		isgreen: false,
-		isinvisible: false,
-		isvisible: true,
 		message: ''
 	},
 	methods: {
-		show () {
-			this.$emit('show')
-			console.log('show')
-			this.isvisible = false
+		showText () {
+			console.log('text')
+			this.imageVisible = false
+			this.textVisible = true
 		},
-		hide () {
-			this.$emit('hide')
-			console.log('hide')
-			this.isvisible = true
+		showImage () {
+			console.log('image')
+			this.imageVisible = true
+			this.textVisible = false
 		},
 	},
 	computed: {
 		cls: function () {
 			console.log('cls called')
 			return this.isgreen === true ? 'is-green' : 'is-blue'
-		},
-		clsimg: function () {
-			console.log('clsimg called')
-			return this.isinvisible === true ? 'is-invisible' : 'is-blue'
 		}
 	}
 });
